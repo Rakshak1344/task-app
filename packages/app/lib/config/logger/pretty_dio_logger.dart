4284fd69caa3bfb566d4747d284361be155dcc3d
Rakshak1344 <dev.rakshith14@gmail.com>
2026-09-11 19:06:51 +0530

    pretty dio logger

---
+++ packages/app/lib/config/logger/pretty_dio_logger.dart
@@ -0,0 +1,17 @@
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'pretty_dio_logger.g.dart';
+
+@riverpod
+PrettyDioLogger prettyDioLogger(Ref ref) {
+  return PrettyDioLogger(
+    requestHeader: true,
+    requestBody: true,
+    responseBody: true,
+    responseHeader: false,
+    error: true,
+    compact: false,
+    maxWidth: 90,
+  );
+}
