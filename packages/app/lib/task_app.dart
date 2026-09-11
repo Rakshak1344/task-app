fdc27604f862e9fdd816a34dcd1ccc7721d64331
Rakshak1344 <dev.rakshith14@gmail.com>
2026-09-11 19:08:54 +0530

    Splash app, and task app setup

---
+++ packages/app/lib/task_app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class TaskApp extends ConsumerStatefulWidget {
+  const TaskApp({super.key});
+
+  @override
+  ConsumerState createState() => _TaskAppState();
+}
+
+class _TaskAppState extends ConsumerState<TaskApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router();
+  }
+}
