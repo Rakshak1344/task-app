27cb983df2daf3d906b7d21b58824713e08987e8
Rakshak1344 <dev.rakshith14@gmail.com>
2026-09-11 19:29:49 +0530

    material app

--- packages/app/lib/task_app.dart
+++ packages/app/lib/task_app.dart
@@ -1,3 +1,4 @@
+import 'package:app/navigation/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -9,10 +10,15 @@ class TaskApp extends ConsumerStatefulWidget {
 }
 
 class _TaskAppState extends ConsumerState<TaskApp> {
+  var goRouter = ref.watch(goRouterProvider);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-
+      debugShowCheckedModeBanner: false,
+      title: "TaskApp",
+      themeMode: ThemeMode.light,
+      routerConfig: goRouter,
     );
   }
 }
